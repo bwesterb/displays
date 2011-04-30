437be13b53fa7a0bce993bdb1f6f249dcf77e847
Bas Westerbaan <bas@westerbaan.name>
2011-04-30 22:51:03 +0200

    printtable: fix three typos
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- src/printtable.py
+++ src/printtable.py
@@ -5,16 +5,17 @@ def layout_table(table):
         ret = []
         for row in table:
                 if len(row) > len(ret):
-                        ret += [0] * len(row) - len(ret)
+                        ret += [0] * (len(row) - len(ret))
                 for n, field in enumerate(row):
                         ret[n] = max(ret[n], len(field))
+        return ret
 
 def sup_of_layouts(layout1, layout2):
         """ Return the least layout compatible with layout1 and layout2 """
         if len(layout1) > len(layout2):
                 layout1, layout2 = layout2, layout1
         if len(layout1) < len(layout2):
-                layout1 += [0] * len(layout2) - len(layout1)
+                layout1 += [0] * (len(layout2) - len(layout1))
         return [max(layout1[i], layout2[i]) for i in xrange(len(layout1))]
 
 def print_table(table, layout=None):
@@ -28,5 +29,5 @@ def print_table(table, layout=None):
                 for n, field in enumerate(row):
                         if n != 0:
                                 tmp += ' '
-                        tmp += " "*(layout[n] - len(field))
+                        tmp += " "*(layout[n] - len(field)) + field
                 print tmp
