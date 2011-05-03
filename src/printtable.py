735c7f9d133adf7ec0b962e86ef0245fc094d544
Bas Westerbaan <bas@westerbaan.name>
2011-05-03 18:24:45 +0200

    printtable: add support for per-column alignment
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- src/printtable.py
+++ src/printtable.py
@@ -18,16 +18,35 @@ def sup_of_layouts(layout1, layout2):
                 layout1 += [0] * (len(layout2) - len(layout1))
         return [max(layout1[i], layout2[i]) for i in xrange(len(layout1))]
 
-def print_table(table, layout=None):
+def print_table(table, layout=None, alignment=None):
         """ Print the table
 
-        If layout is not specified it is computed with layout_table """
+        If layout is not specified it is computed with layout_table.
+
+        alignment is a iterable of characters to specify how columns should
+        be aligned. For instance, "lrc" will align the first column to the
+        left, the second to the right and the last column will be centered.
+        
+        If no alignment is specified, right alignment is assumed. """
         if layout is None:
                 layout = layout_table(table)
+        alignment = tuple() if alignment is None else tuple(alignment) 
+        if len(alignment) < len(layout):
+                alignment += ('r',) * (len(layout) - len(alignment))
         for row in table:
                 tmp = ''
                 for n, field in enumerate(row):
                         if n != 0:
                                 tmp += ' '
-                        tmp += " "*(layout[n] - len(field)) + field
+                        if alignment[n] == 'r':
+                                tmp += " "*(layout[n] - len(field)) + field
+                        elif alignment[n] == 'l':
+                                tmp += field + " "*(layout[n] - len(field))
+                        elif alignment[n] == 'c':
+                                lspace = (layout[n] - len(field)) / 2
+                                rspace = layout[n] - len(field) - lspace
+                                tmp += " "*lspace + field + " "*rspace
+                        else:
+                                raise ValueError, \
+                                        "Unknown alignment %s"%alignment[n]
                 print tmp
